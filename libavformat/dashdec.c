c08e74224efae8491226afae558762da3e680f9e
Rostislav <azebaku98@gmail.com>
2026-07-29 17:45:26 +0300

    feat: backported muxed av per single rep from upstream 8fad6dcfd906485cffc9becab8b4071f46224f20 commit for DASH, not tested

--- libavformat/dashdec.c
+++ libavformat/dashdec.c
@@ -88,7 +88,8 @@ struct representation {
     char *lang;
     int bandwidth;
     AVRational framerate;
-    AVStream *assoc_stream; /* demuxer stream associated with this representation */
+    AVStream **assoc_stream; /* demuxer streams associated with this representation */
+    int nb_assoc_stream;
 
     int n_fragments;
     struct fragment **fragments; /* VOD list of fragment for profile */
@@ -376,6 +377,7 @@ static void free_representation(struct representation *pls)
         avformat_close_input(&pls->ctx);
     }
 
+    av_freep(&pls->assoc_stream);
     av_freep(&pls->url_template);
     av_freep(&pls->lang);
     av_freep(&pls->id);
@@ -1983,7 +1985,7 @@ static int open_demux_for_component(AVFormatContext *s, struct representation *p
         if (!st)
             return AVERROR(ENOMEM);
 
-        st->id = i;
+        st->id = i + pls->stream_index;
 
         ret = avcodec_parameters_copy(st->codecpar, ist->codecpar);
         if (ret < 0)
@@ -2084,12 +2086,12 @@ static int dash_read_header(AVFormatContext *s)
             if (ret < 0)
                 return ret;
         }
+        rep->stream_index = stream_index;
         ret = open_demux_for_component(s, rep);
 
         if (ret)
             return ret;
-        rep->stream_index = stream_index;
-        ++stream_index;
+        stream_index += rep->ctx->nb_streams;
     }
 
     if(c->n_audios)
@@ -2102,12 +2104,12 @@ static int dash_read_header(AVFormatContext *s)
             if (ret < 0)
                 return ret;
         }
+        rep->stream_index = stream_index;
         ret = open_demux_for_component(s, rep);
 
         if (ret)
             return ret;
-        rep->stream_index = stream_index;
-        ++stream_index;
+        stream_index += rep->ctx->nb_streams;
     }
 
     if (c->n_subtitles)
@@ -2120,12 +2122,12 @@ static int dash_read_header(AVFormatContext *s)
             if (ret < 0)
                 return ret;
         }
+        rep->stream_index = stream_index;
         ret = open_demux_for_component(s, rep);
 
         if (ret)
             return ret;
-        rep->stream_index = stream_index;
-        ++stream_index;
+        stream_index += rep->ctx->nb_streams;
     }
 
     if (!stream_index)
@@ -2138,27 +2140,45 @@ static int dash_read_header(AVFormatContext *s)
 
     for (i = 0; i < c->n_videos; i++) {
         rep = c->videos[i];
-        av_program_add_stream_index(s, 0, rep->stream_index);
-        rep->assoc_stream = s->streams[rep->stream_index];
+        rep->assoc_stream = av_malloc_array(rep->ctx->nb_streams, sizeof(*rep->assoc_stream));
+        if (!rep->assoc_stream)
+            return AVERROR(ENOMEM);
+        rep->nb_assoc_stream = rep->ctx->nb_streams;
+        for (int j = 0; j < rep->ctx->nb_streams; j++) {
+            av_program_add_stream_index(s, 0, rep->stream_index + j);
+            rep->assoc_stream[j] = s->streams[rep->stream_index + j];
+        }
         if (rep->bandwidth > 0)
-            av_dict_set_int(&rep->assoc_stream->metadata, "variant_bitrate", rep->bandwidth, 0);
-        move_metadata(rep->assoc_stream, "id", &rep->id);
+            av_dict_set_int(&rep->assoc_stream[0]->metadata, "variant_bitrate", rep->bandwidth, 0);
+        move_metadata(rep->assoc_stream[0], "id", &rep->id);
     }
     for (i = 0; i < c->n_audios; i++) {
         rep = c->audios[i];
-        av_program_add_stream_index(s, 0, rep->stream_index);
-        rep->assoc_stream = s->streams[rep->stream_index];
+        rep->assoc_stream = av_malloc_array(rep->ctx->nb_streams, sizeof(*rep->assoc_stream));
+        if (!rep->assoc_stream)
+            return AVERROR(ENOMEM);
+        rep->nb_assoc_stream = rep->ctx->nb_streams;
+        for (int j = 0; j < rep->ctx->nb_streams; j++) {
+            av_program_add_stream_index(s, 0, rep->stream_index + j);
+            rep->assoc_stream[j] = s->streams[rep->stream_index + j];
+        }
         if (rep->bandwidth > 0)
-            av_dict_set_int(&rep->assoc_stream->metadata, "variant_bitrate", rep->bandwidth, 0);
-        move_metadata(rep->assoc_stream, "id", &rep->id);
-        move_metadata(rep->assoc_stream, "language", &rep->lang);
+            av_dict_set_int(&rep->assoc_stream[0]->metadata, "variant_bitrate", rep->bandwidth, 0);
+        move_metadata(rep->assoc_stream[0], "id", &rep->id);
+        move_metadata(rep->assoc_stream[0], "language", &rep->lang);
     }
     for (i = 0; i < c->n_subtitles; i++) {
         rep = c->subtitles[i];
-        av_program_add_stream_index(s, 0, rep->stream_index);
-        rep->assoc_stream = s->streams[rep->stream_index];
-        move_metadata(rep->assoc_stream, "id", &rep->id);
-        move_metadata(rep->assoc_stream, "language", &rep->lang);
+        rep->assoc_stream = av_malloc_array(rep->ctx->nb_streams, sizeof(*rep->assoc_stream));
+        if (!rep->assoc_stream)
+            return AVERROR(ENOMEM);
+        rep->nb_assoc_stream = rep->ctx->nb_streams;
+        for (int j = 0; j < rep->ctx->nb_streams; j++) {
+            av_program_add_stream_index(s, 0, rep->stream_index + j);
+            rep->assoc_stream[j] = s->streams[rep->stream_index + j];
+        }
+        move_metadata(rep->assoc_stream[0], "id", &rep->id);
+        move_metadata(rep->assoc_stream[0], "language", &rep->lang);
     }
 
     return 0;
@@ -2170,7 +2190,10 @@ static void recheck_discard_flags(AVFormatContext *s, struct representation **p,
 
     for (i = 0; i < n; i++) {
         struct representation *pls = p[i];
-        int needed = !pls->assoc_stream || pls->assoc_stream->discard < AVDISCARD_ALL;
+        int needed = !pls->nb_assoc_stream;
+
+        for (int j = 0; j < pls->nb_assoc_stream; j++)
+            needed |= pls->assoc_stream[j]->discard < AVDISCARD_ALL;
 
         if (needed && !pls->ctx) {
             pls->cur_seg_offset = 0;
@@ -2238,7 +2261,7 @@ static int dash_read_packet(AVFormatContext *s, AVPacket *pkt)
         if (ret >= 0) {
             /* If we got a packet, return it */
             cur->cur_timestamp = av_rescale(pkt->pts, (int64_t)cur->ctx->streams[0]->time_base.num * 90000, cur->ctx->streams[0]->time_base.den);
-            pkt->stream_index = cur->stream_index;
+            pkt->stream_index += cur->stream_index;
             return 0;
         }
         if (cur->is_restart_needed) {
